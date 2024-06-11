a2e2c0e363c838c3daf6ac470a12dcfaf8e4e995
Анастасия Романова <>
2024-06-11 17:50:56 +0300

    impl project

---
+++ WBApp/WBApp/WBAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WBAppApp.swift
+//  WBApp
+//
+//  Created by Анастасия Романова on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WBAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
